1ba43428d0a183654363422bfe67569767a8f409
F4UST-O3 <pantallanojojo1994@gmail.com>
2026-05-01 23:09:18 +0800

    Done playing and updating the 05-enum.ts file

--- 05-enum.ts
+++ 05-enum.ts
@@ -2,6 +2,7 @@
     enum Role {
         // SuperAdmin,
         // SuperAdmin = 1,
+        // SuperAdmin = 0, // I added this for let userRole: Role = 0; to work
         SuperAdmin = 'SuperAdmin',
         // Teacher,
         Teacher = 'Teacher',
@@ -11,4 +12,32 @@
 
     // let userRole: Role = 0;
     let userRole: Role = Role.SuperAdmin;
-})
+    // let userRole: Role = Role.Student;
+    console.log(userRole) // output: SuperAdmin
+
+})(); // invocator added to run/execute the code
+
+
+
+
+// function named IIFE w/ enum
+// (function(user) {
+//     enum Role {
+//         // SuperAdmin,
+//         // SuperAdmin = 1,
+//         // SuperAdmin = 0, // I added this for let userRole: Role = 0; to work
+//         SuperAdmin = 'SuperAdmin',
+//         // Teacher,
+//         Teacher = 'Teacher',
+//         // Student,
+//         Student = 'Student',
+//     }
+
+//     // let userRole: Role = 0;
+//     // let userRole: Role = Role.SuperAdmin;
+//     let userRole: Role = Role.Student;
+//     console.log(userRole)
+//     console.log('Hello', user+'!', 'Your role is', userRole)
+
+// })('Jori'); // I added my name inside the invocator to call the declared local function(user)
+
