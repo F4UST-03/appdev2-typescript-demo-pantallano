9b8e42115c227e047452352be556d5ac255cab06
F4UST-O3 <pantallanojojo1994@gmail.com>
2026-05-01 23:54:05 +0800

    Done playing and updating the 06-alternative-to-enum.ts file

--- 06-alternative-to-enum.ts
+++ 06-alternative-to-enum.ts
@@ -1,11 +1,31 @@
 (() => {
     // Literal Types (Alternative to Enum)
-    let userRole: 'admin' | 'guess' | 'teacher' | 'student' = 'admin'
+    let userRole, anotherUserRole: 'superadmin' | 'admin' | 'guest' | 'teacher' | 'student' = 'admin' 
+    // I added anotherUserRole variable and added 'superadmin' to it to make it different from userRole
 
-    userRole = 'guess'
-    // anotherUserRole = 'superadmin'
+    userRole = 'admin'
+    anotherUserRole = 'superadmin'
+    console.log(userRole, anotherUserRole)
 
-    function access(role: 'admin' | 'guess' | 'teacher' | 'student') {
-        // ...
+    function access(role: 'admin' | 'guest' | 'teacher' | 'student') {
+        // I added conditional statements to use role parameter inside access() function.
+        // if (role === 'admin') 
+        //     console.log('Admin!')
+        // else if (role === 'guest') 
+        //     console.log('Guest!')
+        // else if (role === 'teacher')
+        //     console.log('Teacher!')
+        // else if (role === 'student') 
+        //     console.log('Student!')
+        
+        // or just using === operator to compare
+        role === 'admin'
+        role === 'guest'
+        role === 'teacher'
+        role === 'student'
+        console.log(role+'!')
     }
-})
+    access('student') // output: student
+})() // Invocator, added to run/execute the code. 
+// I run the code using node.js in the terminal (npx tsx 06-alternative-to-enum.ts / npx ts-node 06-alternative-to-enum.ts)
+
