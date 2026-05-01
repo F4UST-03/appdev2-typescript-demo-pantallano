3b0acb6982e3a0ed331eba2720126c669c74c84a
F4UST-O3 <pantallanojojo1994@gmail.com>
2026-05-01 20:45:24 +0800

    Done playing and updating the 02-vanilla-js-types.js

--- 02-vanilla-js-types.js
+++ 02-vanilla-js-types.js
@@ -1,3 +1,7 @@
-let userName1 = "Elmer";
+let userName1 = 'Jori'; // string
+let userAge = 8; // number
+let isAdult = true; // boolean
+let userGradeSample = 90.550; // output: number
 
-console.log(typeof userName1)
+console.log(typeof userName1, typeof userAge, typeof isAdult)
+// output: string number boolean
